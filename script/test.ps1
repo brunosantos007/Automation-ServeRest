9c46def7544ce1f37fdded51e2829ff3077b0637
Bruno Santos <brunogsantoss@outlook.com>
2024-05-27 23:05:00 -0300

    Trying to make automation continuous

--- script/test.ps1
+++ script/test.ps1
@@ -2,6 +2,6 @@ Param(
   [string]$BROWSER,
   [string]$TAG
 )
-docker run -v "$(pwd):/opt/jenkins" -e BROWSER=$BROWSER -e TAG=$TAG -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
+docker run -v "$(pwd):/opt/jenkins/Gemfile.lock" -e BROWSER=$BROWSER -e TAG=$TAG -P --name container-ruby  --link selenium-hub:selenium-hub cucumber/cucumber
 docker rm -f container-ruby
 docker rm -f selenium-hub
